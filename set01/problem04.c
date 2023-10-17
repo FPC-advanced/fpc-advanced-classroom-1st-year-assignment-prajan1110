557f22265fe0b5317fa41f6427d975ae12311fa1
Prajan Kumar <prajank.bsc23@rvu.edu.in>
2023-10-17 12:15:58 +0530

    committed 10/17/2023, 12:15:57 PM

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void input(int *a, int *b);
-void add(int *a, int *b, int *sum);
-void output(int *a, int *b, int *sum);
+void add(int a, int b, int *sum);
+void output(int a, int b, int sum);
 
 int main()
 {
