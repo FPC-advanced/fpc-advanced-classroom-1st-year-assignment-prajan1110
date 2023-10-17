edbb3a5b5699298d0b969df19994a0652a53f8c5
Prajan Kumar <prajank.bsc23@rvu.edu.in>
2023-10-17 12:16:58 +0530

    committed 10/17/2023, 12:16:57 PM

--- set01/problem04.c
+++ set01/problem04.c
@@ -6,9 +6,9 @@ void output(int a, int b, int sum);
 int main()
 {
     int *a,*b,*sum;
-    input(&a,&b);
-    add(&a,&b,&sum);
-    output(&a,&b,&sum);
+    input(a,b);
+    add(a,b,sum);
+    output(a,b,sum);
     return 0;
 }
 void input(int *a,int *b)
